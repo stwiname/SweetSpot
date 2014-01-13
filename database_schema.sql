e918c71ba2dfdbd38a1a31c5ebbf3816f89a55e8
stwiname <skott.twiname@gmail.com>
2014-01-13 16:23:05 +1300

    Minor updates

--- database_schema.sql
+++ database_schema.sql
@@ -1,10 +1,13 @@
 CREATE TABLE  `sweet_spot_test`.`client` (
 `rowid` INT NOT NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY ,
-`name` TEXT NOT NULL ,
-`email` TEXT NOT NULL ,
+`name` VARCHAR(255) NOT NULL ,
+`email` VARCHAR(255) NOT NULL ,
 `password` TEXT NOT NULL ,
+`salt` TEXT NOT NULL ,
 `phone` INT NOT NULL ,
-`join_date` DATETIME NOT NULL
+`join_date` DATETIME NOT NULL,
+UNIQUE (`name`),
+UNIQUE (`email`)
 ) ENGINE = INNODB;
 
 CREATE TABLE  `sweet_spot_test`.`tier` (
@@ -14,7 +17,7 @@ CREATE TABLE  `sweet_spot_test`.`tier` (
 `price` DECIMAL NOT NULL ,
 `tier_date` DATE NOT NULL ,
 `in_use` BOOL NOT NULL,
-`spaces_used` INT NOT NULL DAFAULT '0',
+`tier_limit` INT NOT NULL DAFAULT '0',
 `city` TEXT NOT NULL,
 UNIQUE (`tier_number`, `tier_date`)
 ) ENGINE = INNODB;
@@ -23,14 +26,14 @@ CREATE TABLE IF NOT EXISTS `event` (
   `rowid` INT NOT NULL AUTO_INCREMENT,
   `name` TEXT NOT NULL,
   `image` TEXT NOT NULL COMMENT 'is a path_name',
-  `event_time` datetime NOT NULL,
+  `event_date` date NOT NULL,
+  `event_time` time NOT NULL,
   `description` TEXT NOT NULL,
   `region` TEXT NOT NULL,
   `address` TEXT NOT NULL,
   `attendees` INT NOT NULL DEFAULT '0',
   `client` INT NOT NULL,
   `tier` INT NOT NULL,
-  `price` DECIMAL NOT NULL,
   PRIMARY KEY (`rowid`),
   CONSTRAINT event_client FOREIGN KEY (`client`) REFERENCES `client` (`rowid`),
   CONSTRAINT event_tier FOREIGN KEY (`tier`) REFERENCES `tier` (`rowid`)
